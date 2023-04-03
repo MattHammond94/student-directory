608903808321da275c87b82d68a73e31c17a703e
Matt Hammond <mjhammond94@gmail.com>
2023-04-03 18:46:44 +0000

    Refactor

--- directory.rb
+++ directory.rb
@@ -1,19 +1,21 @@
+#Putting this gang of naughty witch stabbers into a nice tidy array
+students = [
+  "Dr. Hannival Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+  ]
+
 puts "The students of Villains Academy"
-13.times { print "-" }
-#^^^ I used newline below after implemeting a method above ^^^
-puts "\nDr.Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-#Absolute pack of savages studying ere m8!
-print "Overall, we have " 
-print 11 
-puts " great students"
-#Right you are! 
+puts "-------------"
+students.each { |student| puts student }
+#^^We iterating now baby!^^
+puts "Overall, we have #{students.count} great students"
+#Plenty of savages studying ere m8!
