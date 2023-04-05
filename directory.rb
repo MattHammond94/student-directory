7edee27454375a1da22c4b9eddce813b46bd2546
MattyMooMilk <mjhammond94@gmail.com>
2023-04-05 15:21:25 +0000

    interactive menu refactored - instance variable added

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+@students = []
+
 def input_students
   # An array of acceptable cohorts defined as a global variable to allow multiple use
   $acceptable_cohorts = [:january, :february, :march, :april, :may, :june,
@@ -7,8 +9,7 @@ def input_students
   puts "Please enter the name of the students"
   puts "To finish, just hit return twice"
   
-  students = []
-  name = gets.strip
+  name = gets.chomp
   
   while !name.empty? do
     # Added further statements to allow user to input multiple values to new keys in hash
@@ -26,10 +27,10 @@ def input_students
     puts "Finally, which country is #{name} from?"
     country = gets.strip.to_sym
     
-    students << {name: name, cohort: cohort, hobby: hobby, cob: country}
-    student_count = "Now we have #{students.count} student"
+    @students << {name: name, cohort: cohort, hobby: hobby, cob: country}
+    student_count = "Now we have #{@students.count} student"
     
-      if students.count == 1
+      if @students.count == 1
         puts student_count
       else   
         puts student_count + "s"
@@ -37,7 +38,6 @@ def input_students
     puts "Add another student or hit enter to finish."
     name = gets.strip
   end
-  students
 end
   
 # Center method added to outputs in header method  
@@ -47,18 +47,18 @@ def print_header
 end
 
 # Control flow added to print method to account for empty arrays
-def print(students)
-  if students.empty? 
+def print_students_list
+  if @students.empty? 
     puts "There are currrently no students at the Villains Academy".center(100)
   else
-    students.each do |student| 
+    @students.each do |student| 
       puts "#{student[:name]} (#{student[:cohort]} cohort)".center(100)
     end
   end
 end
 
 # Print cohort method added to puts all names within selected cohort
-def print_by_cohort(students)
+def print_by_cohort
   puts "select a cohort month from 1-12".center(100)
   cohort = gets.strip
   while cohort.to_i < 1 || cohort.to_i > 12
@@ -66,53 +66,55 @@ def print_by_cohort(students)
     cohort = gets.strip
   end 
   
-  students.each do |hash| 
+  @students.each do |hash| 
     hash.map { |k, v| puts hash[:name].center(100) if $acceptable_cohorts[cohort.to_i - 1] == v }
   end
 end
   
 # Control flow added to footer method to control output  
-def print_footer(students)
-  if students.count < 1
+def print_footer
+  if @students.count < 1
     puts "Our most evilest(MF the Super Villain) had better start recruiting because we have no students."
-  elsif students.count == 1
-    puts "Overall, we have #{students.count} great student".center(100)
-  elsif students.count <= 7
-    puts "Overall, we have #{students.count} great students".center(100)
+  elsif @students.count == 1
+    puts "Overall, we have #{@students.count} great student".center(100)
+  elsif @students.count <= 7
+    puts "Overall, we have #{@students.count} great students".center(100)
   else 
-    puts "These #{students.count} are a questional bunch".center(100)
+    puts "These #{@students.count} are a questional bunch".center(100)
   end
 end
 
-# Adding an interactive menu:
+# Refactoring the interactive menu into smaller methods:
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else 
+      puts "I don't know what you mean, try again"
+  end
+end
+
 def interactive_menu
-  students = []
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
-    
-    case selection
-     when "1"
-       students = input_students
-     when "2"
-       print_header
-       print(students)
-       print_footer(students)
-     when "9"
-       exit
-     else
-       puts "I don't know what you mean't, try again"
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
-
-# Methods called
-# students = input_students
-# print_header
-# print(students)
-# print_footer(students)
-# print_by_cohort(students)
 
 interactive_menu
