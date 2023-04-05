da96519ae1ccceb2902489ce8a29a127b38012eb
MattyMooMilk <mjhammond94@gmail.com>
2023-04-05 07:00:52 +0000

    Typos corrected and notes added

--- directory_2.rb
+++ directory_2.rb
@@ -2,17 +2,17 @@
 # Methods have been added to complete exercises 1-6.
 
 students = [
-  {name: "Dr. Hannibal Lecter", hobbies: "Chianti", height: 6, cohort: :november},
-  {name: "Darth Vader", hobbies: "Podracing", height: 6.9, cohort: :november},
-  {name: "Nurse Ratched", hobbies: "Birdwatching", height: 5.6, cohort: :november},
-  {name: "Michael Corleone", hobbies: "Pizza", height: 5.9, cohort: :november},
-  {name: "Alex DeLarge", hobbies: "Ultraviolence", height: 5.9, cohort: :november},
-  {name: "The Wicked Witch of the West", hobbies: "Her prettys", height: 5.6, cohort: :november},
-  {name: "Terminator", hobbies: "John Connor", height: 6.3, cohort: :november},
-  {name: "Freddy Krueger", hobbies: "Nightmares", height: 5.9, cohort: :november},
-  {name: "The Joker", hobbies: "Magic", height: 6.0, cohort: :november},
-  {name: "Joffrey Baratheon", hobbies: "Crossbowin", height: 5.5, cohort: :november},
-  {name: "Norman Bates", hobbies: "Motels", height: 6.1, cohort: :november}
+  {name: "Dr. Hannibal Lecter", hobbies: ["Chianti", "Flesh"], height: 6, cohort: :november},
+  {name: "Darth Vader", hobbies: ["Podracing", "Laser-Swords"], height: 6.9, cohort: :november},
+  {name: "Nurse Ratched", hobbies: ["Birdwatching", "Lobotomys"], height: 5.6, cohort: :november},
+  {name: "Michael Corleone", hobbies: ["Pizza", "Gorlomi"], height: 5.9, cohort: :november},
+  {name: "Alex DeLarge", hobbies: ["Ultraviolence", "Milk"], height: 5.9, cohort: :november},
+  {name: "The Wicked Witch of the West", hobbies: ["Her prettys", "Broom-Riding"], height: 5.6, cohort: :november},
+  {name: "Terminator", hobbies: ["John Connor", "Sarah Connor"], height: 6.3, cohort: :november},
+  {name: "Freddy Krueger", hobbies: ["Nightmares", "Striped-Attire"], height: 5.9, cohort: :november},
+  {name: "The Joker", hobbies: ["Magic", "Pranks"], height: 6.0, cohort: :november},
+  {name: "Joffrey Baratheon", hobbies: ["Crossbowin", "Tourneys"], height: 5.5, cohort: :november},
+  {name: "Norman Bates", hobbies: ["Motels", "Showers"], height: 6.1, cohort: :november}
   ]
 # ^^^Exercise no.5^^^
 
@@ -32,7 +32,7 @@ def selected_letter(students)
   end
 end
 # ^^^Exercise no.2^^^
-# This method can be greatly improved by adding an if/else to ensure that the 
+# This method can be greatly improved by using control flow to ensure that the 
 # input is never more than 1 letter. I will return to this exercise.
 # Could even create an array of acceptable letters by pushing all the [0] letters 
 # from each student[:name] - making these letters the only acceptable inputs.
@@ -67,3 +67,4 @@ selected_letter(students)
 shorter_than_twelve(students)
 rewrite(students)
 print_centered(students)
+# ^^^Methods called^^^
