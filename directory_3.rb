7de657d9e601bf209a23d6ecc35ad057b41ddd55
MattyMooMilk <mjhammond94@gmail.com>
2023-04-06 10:30:17 +0000

    First refactor

--- directory_3.rb
+++ directory_3.rb
@@ -1,88 +1,10 @@
 # This program is a copy of the directory.rb file upon completion of step 13
 # This codebase will be refactored accordingly to complete the tasks in step 14
-
 @students = []
+@acceptable_cohorts = [:january, :february, :march, :april, :may, :june,
+                       :july, :august, :september, :october, :november,
+                       :december]
 
-def input_students
-  puts "Please enter the name of the students"
-  puts "To finish, just hit return twice"
-  
-  name = STDIN.gets.chomp
-  
-  while !name.empty? do
-    # Added further statements to allow user to input multiple values to new keys in hash
-    puts "Which cohort does #{name} belong to?"
-    # This loop only allows users to input a month as a :cohort value 
-    # This can also be edited for the other Keys in our hash if required
-    cohort = STDIN.gets.chomp.to_sym
-    until (@acceptable_cohorts.select { |month| month == cohort }.empty?) == false do
-      puts "This cohort is invalid"
-      cohort = STDIN.gets.chomp.to_sym
-    end
-    
-    puts "What is #{name}s favourite hobby?"
-    hobby = STDIN.gets.chomp.to_sym
-    puts "Finally, which country is #{name} from?"
-    country = STDIN.gets.chomp.to_sym
-    
-    @students << {name: name, cohort: cohort, hobby: hobby, cob: country}
-    student_count = "Now we have #{@students.count} student"
-    
-      if @students.count == 1
-        puts student_count
-      else   
-        puts student_count + "s"
-      end
-    puts "Add another student or hit enter to finish."
-    name = STDIN.gets.chomp
-  end
-end
-  
-# Center method added to outputs in header method  
-def print_header
-  puts "The Students of Villains Academy"
-  puts "-------------"
-end
-
-# Control flow added to print method to account for empty arrays
-def print_students_list
-  if @students.empty? 
-    puts "There are currrently no students at the Villains Academy"
-  else
-    @students.each do |student| 
-      puts "#{student[:name]} (#{student[:cohort]} cohort)"
-    end
-  end
-end
-
-# Print cohort method added to puts all names within selected cohort
-def print_by_cohort
-  puts "select a cohort month from 1-12"
-  cohort = STDIN.gets.chomp
-  while cohort.to_i < 1 || cohort.to_i > 12
-    puts "invalid input"
-    cohort = STDIN.gets.chomp
-  end 
-  
-  @students.each do |hash| 
-    hash.map { |k, v| puts hash[:name] if @acceptable_cohorts[cohort.to_i - 1] == v }
-  end
-end
-  
-# Control flow added to footer method to control output  
-def print_footer
-  if @students.count < 1
-    puts "Our most evilest(MF the Super Villain) had better start recruiting because we have no students."
-  elsif @students.count == 1
-    puts "Overall, we have #{@students.count} great student"
-  elsif @students.count <= 7
-    puts "Overall, we have #{@students.count} great students"
-  else 
-    puts "These #{@students.count} are a questionable bunch"
-  end
-end
-
-# Refactoring the interactive menu into smaller methods:
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
@@ -91,11 +13,11 @@ def print_menu
   puts "9. Exit"
 end
 
-# Amended back
-def show_students
-  print_header
-  print_students_list
-  print_footer
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
 end
 
 def process(selection)
@@ -115,10 +37,61 @@ def process(selection)
   end
 end
 
-def interactive_menu
-  loop do
-    print_menu
-    process(STDIN.gets.chomp)
+def input_students
+  puts "Please enter the name of the students"
+  puts "To finish, just hit return twice"
+  
+  name = STDIN.gets.chomp
+  
+  while !name.empty? do
+    puts "Which cohort does #{name} belong to?"
+    cohort = STDIN.gets.chomp.to_sym
+    until @acceptable_cohorts.select { |month| month == cohort }.empty? == false do
+      puts "This cohort is invalid"
+      cohort = STDIN.gets.chomp.to_sym
+    end
+    @students << {name: name, cohort: cohort}
+    student_count = "Now we have #{@students.count} student"
+      if @students.count == 1
+        puts student_count
+      else   
+        puts student_count + "s"
+      end
+    puts "Add another student or hit enter to finish."
+    name = STDIN.gets.chomp
+  end
+end
+  
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+  
+def print_header
+  puts "The Students of Villains Academy"
+  puts "-------------"
+end
+
+def print_students_list
+  if @students.empty? 
+    puts "There are currrently no students at the Villains Academy"
+  else
+    @students.each do |student| 
+      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
+end
+  
+def print_footer
+  if @students.count < 1
+    puts "Our most evilest(MF the Super Villain) had better start recruiting because we have no students."
+  elsif @students.count == 1
+    puts "Overall, we have #{@students.count} great student"
+  elsif @students.count <= 7
+    puts "Overall, we have #{@students.count} great students"
+  else 
+    puts "These #{@students.count} are a questionable bunch"
   end
 end
 
@@ -157,5 +130,6 @@ def try_load_students
   end
 end
 
+# Methods called
 try_load_students
 interactive_menu
